3d9e2afee489569c0ad4956f9b353d00479e43fb
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2014-10-15 15:38:20 +0200

    Adapted for mobile

--- js/project-show.js
+++ js/project-show.js
@@ -14,14 +14,23 @@ $(document).ready(function() {
 		if (mq.matches) 
 		{
 			// window width is less than 768px
-			$(".plx-img").removeClass("plx-img").addClass("plx-img-inactive")
-			$(".plx-float").removeClass("plx-float").addClass("plx-float-inactive")
+			$(".plx-img")
+				.removeClass("plx-img")
+				.addClass("plx-img-inactive")
+				.css("background-position-y", "")
+			$(".plx-float")
+				.removeClass("plx-float")
+				.addClass("plx-float-inactive")
 		}
 		else 
 		{
 			// window width is at least 768px
-			$(".plx-img-inactive").removeClass("plx-img-inactive").addClass("plx-img")
-			$(".plx-float-inactive").removeClass("plx-float-inactive").addClass("plx-float")
+			$(".plx-img-inactive")
+				.removeClass("plx-img-inactive")
+				.addClass("plx-img")
+			$(".plx-float-inactive")
+				.removeClass("plx-float-inactive")
+				.addClass("plx-float")
 		}
 	}
 })
